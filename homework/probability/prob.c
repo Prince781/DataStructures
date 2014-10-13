40244e325cc2b8cee987350fa7d0dbb23b5fa878
Prince781 <princetonf@optonline.net>
2014-10-13 02:58:57 -0400

    Updating probability.

--- homework/probability/prob.c
+++ homework/probability/prob.c
@@ -2,8 +2,9 @@
 #include <stdbool.h>
 #include <stdlib.h>	/* srand, rand */
 #include <time.h>
+#include <math.h>	/* pow */
 
-#define ITERS	10000000UL
+#define ITERS	100UL
 
 /* bad flip with non-uniform distribution */
 bool bad_flip(void);
@@ -11,21 +12,28 @@ bool bad_flip(void);
 /* return uniform distribution */
 bool flip(void);
 
+/* return true with probability m/n */
+bool flop(int m, int n);
+
+int gcd(int a, int b);
+
 int main(int argc, char *argv[]) {
 	double avg = 0;
 	unsigned long n;
+	int num = 300, denom = 1000;
 
 	srand(time(NULL));
 	for (n=0; n<=ITERS; avg /= ++n)
-		avg = n*avg + flip();
+		avg = n*avg + flop(num, denom);
 	
-	printf("after %d iterations, avg = %f\n", ITERS, avg);
+	printf("after %d iterations with true prob (%f), avg = %f\n",
+		ITERS, (float)num / denom, avg);
 
 	return 0;
 }
 
 bool bad_flip(void) {
-	return (rand() % 100 > 1);
+	return (rand() % 100 > 98);
 }
 
 bool flip(void) {
@@ -33,5 +41,21 @@ bool flip(void) {
 
 	r1 = bad_flip();
 	r2 = bad_flip();
-	return (r1 ^ r2 ? r1 : !flip());
+	return (r1 ^ r2 ? r1 : flip());
+}
+
+bool flop(int m, int n) {
+	bool val;
+	int i, f;
+
+	val = false;
+	f = (int) ceil(log((float)(m > n/2 ? (n-m) : m)/n) / log(0.5));
+	for (i=f; i>0; --i)
+		val |= flip();
+	return (m > n/2 ? val : !val);
+}
+
+int gcd(int a, int b) {
+	if (a == 0) return b;
+	return gcd(b%a, a);
 }
