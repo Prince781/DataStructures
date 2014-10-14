434de434bdf607a01db227925c8fe56de1d89a5a
Prince781 <princetonf@optonline.net>
2014-10-13 22:24:06 -0400

    Adding benchmark.

--- homework/probability/prob.c
+++ homework/probability/prob.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>	/* srand, rand */
-#include <time.h>
+#include "prob_benchmark.h"
 
 #define ITERS	10000000UL
 
@@ -15,25 +15,11 @@ bool flip(void);
 bool flop(int m, int n);
 
 int main(int argc, char *argv[]) {
-	double avg = 0;
-	unsigned long n;
-	int num = 300, denom = 1000;
-
 	srand(time(NULL));
-	for (n=0; n<ITERS; avg /= ++n)
-		avg = n*avg + flip();
-	
-	printf("after %d iterations of flip(), avg = %f\n", n, avg);
 
-	avg = 0;
-	for (n=0; n<=ITERS; avg /= ++n) {
-		avg = n*avg + flop(num, denom);
-		printf("iter %d / %d\n", n, ITERS);
-	}
-	
-	printf("after %d iterations of flop() with true prob (%f), avg = %f\n",
-		ITERS, (float)num / denom, avg);
-
+	benchmark_flip_test(flip);
+	benchmark_flop_test_all_p(flop);
+	benchmark_flop_test(flop,3,100);
 	return 0;
 }
 
