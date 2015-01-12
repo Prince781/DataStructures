8f26771940f95b5cf0ffb3cfa046ab3cacdc113a
Prince781 <princetonf@optonline.net>
2015-01-11 23:00:51 -0500

    Adding test data generator.

---
+++ homework/2sat/gendata.c
@@ -0,0 +1,32 @@
+/* gendata.c
+ * generate random 2sat data
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc < 4) {
+		printf("usage: %s [file] [nvalues] [nclauses]\n", argv[0]);
+		return 1;
+	}
+	
+	FILE *fin = fopen(argv[1], "w");
+	int nv = atoi(argv[2]);
+	int nc = atoi(argv[3]);
+	int i, neg1, neg2;
+	int x1, x2;
+
+	fprintf(fin, "%d %d\n", nv, nc);
+	srand(time(NULL));
+	for (i=0; i<nc; ++i) {
+		neg1 = rand() < RAND_MAX/2 ? -1 : 1;
+		neg2 = rand() < RAND_MAX/2 ? -1 : 1;
+		x1 = (int)(((float)rand() / RAND_MAX) * nv);
+		x2 = (int)(((float)rand() / RAND_MAX) * nv);
+		fprintf(fin, "%d %d\n", neg1*x1, neg2*x2);
+	}
+
+	fclose(fin);
+}
