4fe6ca45b54a09145f86359eb143edafd4e93780
Prince781 <princetonf@optonline.net>
2015-03-25 02:04:39 -0400

    Fixed overflow in sym_map; improved machine.

--- homework/tm/turingmachine.c
+++ homework/tm/turingmachine.c
@@ -3,14 +3,16 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/types.h>
-
+#include <malloc.h>
 
 #define TAPESIZE 1024
 #define HALT	-1
 
+static const int debug = 1;
+
 struct tuple {
 	int new_state;
-	char *new_sym;
+	char new_sym;
 	char new_dir;
 };
 
@@ -50,8 +52,8 @@ int main(int argc, char *argv[])
 	int n_tuples;
 	char dir;
 	FILE *program;
-	char *tape_buffer = NULL;
-	size_t tape_size = 0;
+	char *tape_buffer;
+	size_t tape_size = 1024;
 	ssize_t tape_read;
 
 	if (argc < 2) {
@@ -59,26 +61,35 @@ int main(int argc, char *argv[])
 		return 1;
 	} else if ((program = fopen(argv[1], "r")) == NULL) {
 		const char *errstr = strerror(errno);
-		fprintf(stderr, "%s\n", errstr);
+		fprintf(stderr, "%s: %s\n", argv[1], errstr);
 		return 1;
 	}
 	
 	fscanf(program, "%d %d", &n_states, &n_syms);
 	n_tuples = n_states * n_syms;
 	symbols = malloc(n_syms+1);
+	symbols[n_syms] = 0;
 	fscanf(program, "%s", symbols);
 
+	if (debug)
+		printf("states: %d, symbols(%d): %s\n",
+		       n_states, n_syms, symbols);
 	new_states = malloc(sizeof(*new_states) * n_tuples);
 	new_syms = malloc(n_tuples);
 	new_dirs = malloc(n_tuples);
 	for (int i=0; i<n_tuples; ++i) {
-		scanf("%d %c %c", new_states+i, new_syms+i, &dir);
+		fscanf(program, "%d %c %c", new_states+i, new_syms+i, &dir);
 		new_dirs[i] = dir == '+' ? 1 : -1;
+		if (debug)
+			printf("(%d %c) -> (%d %c %c)\n",
+			       i/n_syms, symbols[i%n_syms],
+			       new_states[i], new_syms[i], dir);
 	}
 
 	tm = tm_new(symbols, new_states, new_syms, new_dirs, n_states, n_syms);
-
+	
 	// read tape from standard in
+	tape_buffer = malloc(tape_size);
 	tape_read = getline(&tape_buffer, &tape_size, stdin);
 	if (tape_read < 1 || tape_buffer[0] == '\n')
 		fprintf(stderr, "no tape given\n");
@@ -92,6 +103,8 @@ int main(int argc, char *argv[])
 	free(new_states);
 	free(new_syms);
 	free(new_dirs);
+
+	return 0;
 }
 
 struct TM *tm_new(const char *symbols,
@@ -107,18 +120,17 @@ struct TM *tm_new(const char *symbols,
 
 	tm->syms = strdup(symbols);
 	tm->n_syms = n_syms;
-	tm->sym_map = malloc(256);
+	tm->sym_map = malloc(sizeof(*tm->sym_map) * 256);
 
 	tm->n_states = n_states;
-	tm->instrcts = malloc(sizeof(*tm->instrcts) * tm->n_tuples);
+	tm->instrcts = malloc(sizeof(*tm->instrcts) * (tm->n_tuples + n_syms));
 	for (int i=0; i<tm->n_tuples; ++i) {
-		char *sym_i = memchr(tm->syms, new_syms[i], n_syms);
-		int p = new_states[i] * n_syms + (sym_i - tm->syms);
-		tm->instrcts[p].new_state = new_states[i];
-		tm->instrcts[p].new_sym = sym_i;
-		tm->instrcts[p].new_dir = new_dirs[i];
-		tm->sym_map[*sym_i] = sym_i - tm->syms;
+		tm->instrcts[i].new_state = new_states[i];
+		tm->instrcts[i].new_sym = new_syms[i];
+		tm->instrcts[i].new_dir = new_dirs[i];
 	}
+	for (char *sym_i = tm->syms; *sym_i; ++sym_i)
+		tm->sym_map[*sym_i] = sym_i - tm->syms;
 
 	tm->tape = NULL;
 	tm->cur_state = -1;
@@ -132,16 +144,26 @@ void tm_init(struct TM *tm, int init_state, int loc, const char *tape)
 	tm->cur_state = init_state;
 	tm->loc = loc;
 	tm->tape = strdup(tape);
+
 }
 
 int tm_step(struct TM *tm)
 {
+	if (tm->cur_state == HALT)
+		return HALT;
 	char sym = tm->tape[tm->loc];
 	int p = tm->cur_state * tm->n_syms + tm->sym_map[sym];
 	struct tuple next = tm->instrcts[p];
 
-	tm->tape[tm->loc] = *next.new_sym;
+	if (debug) {
+		printf("   %*sv\n", tm->loc, "");
+		printf("1. %s\n", tm->tape);
+	}
+
+	tm->tape[tm->loc] = next.new_sym;
 	tm->loc += next.new_dir;
+	if (debug)
+		printf("2. %s\n", tm->tape);
 	return tm->cur_state = next.new_state;
 }
 
